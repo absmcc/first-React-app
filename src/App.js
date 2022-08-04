a790c3aa9f0ceb6415489ae0533704994cfa9074
Maria Redman <mariaredman@Marias-MacBook-Air-2.local>
2022-08-04 11:45:49 -0400

    first react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg"
+import "./App.css"
 
 function App() {
+  let count = 0;
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>My First React App</h1>
+        <p>You clicked the button {count} times.</p>
+        <button onClick={()=> {
+          console.log('I clicked the button');
+          count =count + 1;
+        }}>Click Me</button>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
